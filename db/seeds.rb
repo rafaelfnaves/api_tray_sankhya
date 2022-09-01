3636f1034b2246a48f2189ddc75e069c94f0f567
Rafael Naves <r.naves21@gmail.com>
2022-09-01 16:14:36 -0300

    Create product on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create!(
+  sku: "123SKU45", 
+  active: "S", 
+  price: 59.80,
+  cost: 20.0,
+  ncm: "67049000",
+  name: "Produto de teste",
+  stock: 5,
+  brand: "TEST",
+  weight: 10,
+  height: 10,
+  width: 5,
+  length: 5,
+  description: "Estas folhas protetoras cobrem efetivamente os cílios inferiores enquanto hidratam a frágil pele abaixo dos olhos. Sua fórmula adere com suavidade e firmez
+                a durante a aplicação.\n\nPropriedades das folhas protetoras adesivas em gel:\nIncluí antioxidantes de colágeno, vitaminas e glicerina, aumentando o conforto
+                da cliente durante o procedimento de alongamento dos cílios, tintura e permanente. Coloque o adesivo embaixo da linha dos cílios inferiores, com a parte em ge
+                l para baixo.\n\nNessa posição a cliente terá um efeito refrescante e ajuda a evitar qualquer tipo de inchaço. As folhas adesivas também servem de almofada e
+                proteção da pele durante o procedimento.\n\nQuantidade: 50 Pares"
+)
