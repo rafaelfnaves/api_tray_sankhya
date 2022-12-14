7f0e77fa3074c5955bc5459db31a0fba9f193d06
Rafael Naves <r.naves21@gmail.com>
2022-12-14 13:35:19 -0300

    changed model order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,72 +3,76 @@ class Order < ApplicationRecord
 
    def self.get_order!(id_tray)
       Rails.logger.info "Entrou get_order"
-   
-      token = Auth.access_token!()
-      url = "#{ENV['API_ADDRESS']}/orders/#{id_tray}/complete?access_token=#{token}"
-      Rails.logger.info "URL: #{url}"
-      response = RestClient.get url
-      hash = JSON.parse(response.body)
+      
+      id = Order.find_by_id_tray(id_tray)
+      
+      if id.nil?
+         token = Auth.access_token!()
+         url = "#{ENV['API_ADDRESS']}/orders/#{id_tray}/complete?access_token=#{token}"
+         Rails.logger.info "URL: #{url}"
+         response = RestClient.get url
+         hash = JSON.parse(response.body)
 
-      Rails.logger.info "Response: #{response.code} - #{response.body}"
+         Rails.logger.info "Response: #{response.code} - #{response.body}"
 
-      order = hash["Order"]
-      if order["has_payment"] == "1"
-         customer = Customer.find_by_id_tray(order["customer_id"]) # Consultar qual é cliente na Tray e cadastrar no banco intermediário e conferir se existe no SNK, caso não exista, cadastrar e pegar o CODPARC e salvar no banco
-         if customer.nil?
-            customer = Customer.create_customer!(order["customer_id"], token)
-         end
+         order = hash["Order"]
+         if order["has_payment"] == "1"
+            customer = Customer.find_by_id_tray(order["customer_id"]) # Consultar qual é cliente na Tray e cadastrar no banco intermediário e conferir se existe no SNK, caso não exista, cadastrar e pegar o CODPARC e salvar no banco
+            if customer.nil?
+               customer = Customer.create_customer!(order["customer_id"], token)
+            end
 
-         puts "Cliente: #{customer.name} - CODPARC: #{customer.code_par_snk}"
+            puts "Cliente: #{customer.name} - CODPARC: #{customer.code_par_snk}"
 
-         items = []
-         order["ProductsSold"].each do |pr_sold|
-            product = Product.find_by_id_tray(pr_sold["ProductsSold"]["product_id"])
-            item = {
-               "NUNOTA":{
-               },
-               "CODPROD":{
-                   "$": product.sku.to_i
-               },
-               "QTDNEG":{
-                   "$": pr_sold["ProductsSold"]["quantity"].to_f
-               },
-               "CODLOCALORIG":{
-                   "$":"20100".to_i
-               },
-               "CODVOL":{
-                   "$": product.volume
-               },
-               "VLRUNIT":{
-                   "$": pr_sold["ProductsSold"]["price"].to_f
-               },
-               "PERCDESC":{
-                   "$": "0".to_f
+            items = []
+            order["ProductsSold"].each do |pr_sold|
+               product = Product.find_by_id_tray(pr_sold["ProductsSold"]["product_id"])
+               item = {
+                  "NUNOTA":{
+                  },
+                  "CODPROD":{
+                     "$": product.sku.to_i
+                  },
+                  "QTDNEG":{
+                     "$": pr_sold["ProductsSold"]["quantity"].to_f
+                  },
+                  "CODLOCALORIG":{
+                     "$":"20100".to_i
+                  },
+                  "CODVOL":{
+                     "$": product.volume
+                  },
+                  "VLRUNIT":{
+                     "$": pr_sold["ProductsSold"]["price"].to_f
+                  },
+                  "PERCDESC":{
+                     "$": "0".to_f
+                  }
                }
-            }
-            items << item
-         end
+               items << item
+            end
 
-         puts "Produtos comprados: #{items}"
-         
-         obs = "
-         Frete: #{order["shipment"]},\n
-         Valor do frete: #{order["shipment_value"]},\n
-         Data de entrega: #{order["shipment_date"]},\n 
-         Método de Pagamento:#{order["payment_method"]}
-         "
-         nu_nota = Order.create_snk!(customer.code_par_snk, items, order["payment_date"], obs)
+            puts "Produtos comprados: #{items}"
             
-         self.create!(
-            nu_nota: nu_nota,
-            date: order["date"],
-            total: order["total"],
-            payment_date: order["payment_date"],
-            id_tray: order["id"],
-            customer_id: customer.id,
-         )
+            obs = "
+            Frete: #{order["shipment"]},\n
+            Valor do frete: #{order["shipment_value"]},\n
+            Data de entrega: #{order["shipment_date"]},\n 
+            Método de Pagamento:#{order["payment_method"]}
+            "
+            nu_nota = Order.create_snk!(customer.code_par_snk, items, order["payment_date"], obs)
+               
+            self.create!(
+               nu_nota: nu_nota,
+               date: order["date"],
+               total: order["total"],
+               payment_date: order["payment_date"],
+               id_tray: order["id"],
+               customer_id: customer.id,
+            )
 
-         puts "Finaliza Pedido"    
+            puts "Finaliza Pedido"    
+         end
       end
    end
 
