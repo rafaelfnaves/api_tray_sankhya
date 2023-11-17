9aaa033540bbe088c7fd51669af9b5a7e9ae252f
Rafael Naves <r.naves21@gmail.com>
2023-11-17 19:58:31 -0300

    Adjusts compose

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,13 @@
-set :environment, 'production'
-set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
+set :output, '/var/log/cron.log' # log location
 
-every :day, at: ["06:00 AM", "02:00 PM"] do
+every :day, at: ["07:00 AM", "02:00 PM", "08:00 PM"] do
   rake "db:update_products"
 end
 
-every 3.minutes do
+every 5.minutes do
   rake 'snk:stock_price'
 end
 
-every 7.minutes do
-  rake 'tray:update_products'
-end
-
-every 10.minutes do
+every 5.minutes do
   rake 'tray:get_orders'
 end
