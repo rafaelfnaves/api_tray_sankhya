8ffb714613ce758e8b24b9521c5b52bc28f5ce8a
Rafael Naves <r.naves21@gmail.com>
2024-02-08 13:24:46 -0300

    feat!(jobs): configure whenever save logs each cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,14 @@
-set :output, 'log/cron.log' # log location
+# log location global
+# set :output, 'log/cron.log' 
 
 every 2.hours do
-  rake "db:update_products"
+  rake 'db:update_products', output: 'log/update-products-cron.log'
 end
 
 every 15.minutes do
-  rake 'snk:stock_price'
+  rake 'snk:stock_price', output: 'log/stock-price-cron.log'
 end
 
-every 10.minutes do
-  rake 'tray:get_orders'
+every 5.minutes do
+  rake 'tray:get_orders', output: 'log/orders-cron.log'
 end
